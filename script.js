cbf0c5ccc9effd0f531bee93b620bdc9510e0a1a
kowserahmed121 <kowser.dev@gmail.com>
2024-09-19 15:44:37 +0600

    add many ways change theme

--- script.js
+++ script.js
@@ -12,3 +12,18 @@ const bgYellow = document.getElementById("btn-yellow");
 bgYellow.onclick = function makeYellow() {
   document.body.style.backgroundColor =  "yellow";
 };
+
+const bgSet = document.getElementById("btn-pink");
+bgSet.addEventListener("click", bgPink)
+function bgPink(){
+  document.body.style.backgroundColor = "pink"
+}
+
+const bgAqua = document.getElementById("btn-aqua");
+bgAqua.addEventListener("click", function makeAqua(){
+  document.body.style.backgroundColor = "aqua"
+})
+
+ document.getElementById("btn-tomato").addEventListener("click", function(){
+  document.body.style.backgroundColor = "tomato"
+ })
